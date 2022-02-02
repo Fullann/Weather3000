90b02a563beef9ac4e4c84e9bcebac9ffd01b099
Nathan Füllemann <nathan.fullemann@gmail.com>
2022-02-02 13:48:27 +0100

    [INIT] Load project

---
+++ main.js
@@ -0,0 +1,54 @@
+const api = {
+    key: "7894e4b537169089f63bb25efb31c10c",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+const searchbox = document.querySelector(".Search-box");
+const btnsearch = document.querySelector(".btn-search");
+
+searchbox.addEventListener('keypress', setQuery);
+btnsearch.addEventListener('click', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13 || evt.pointerType == "mouse") {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResult);
+}
+
+function displayResult(weather) {
+    let city = document.querySelector(".city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector(".date");
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector(".temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>℃</span>`;
+
+    let weather_el = document.querySelector(".weather");
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector(".hiLow");
+    hilow.innerText = `${weather.main.temp_min}℃/${weather.main.temp_max}℃`
+
+}
+
+function dateBuilder(d) {
+    let months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
+    let days = ["Dimanhe", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+
+}
